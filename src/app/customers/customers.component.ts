f1e545ead7a5878400413be19487252ade6670ba
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-11-01 12:44:46 +0100

    update customers components

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -12,11 +12,14 @@ export class CustomersComponent implements OnInit {
   constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get("http://localhost:8085/customers").subscribe(data=>{
-      this.customers = data;
-    },error => {
-      console.log(error);
-    })
+    this.http.get("http://localhost:8085/customers").subscribe({
+      next : (data) => {
+        this.customers = data;
+      },
+      error : (err) => {
+        console.log(err);
+      }
+    });
   }
 
 }
